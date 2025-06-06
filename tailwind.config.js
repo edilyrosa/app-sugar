eec60838eb199d1464b9a1b2eca6df9c55041d52
Edily Mora <edilyrosa@gmail.com>
2025-06-06 10:15:44 -0400

    adding confi, style by tailwind and opacity effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,8 +21,39 @@
 
 
 
+
+
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "./app/**/*.{js,ts,jsx,tsx}",
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       screens: {
+//         xs: "380px",
+//         s: "400px"
+//       },
+//     },
+//   },
+//   plugins: [
+//     require('@tailwindcss/aspect-ratio'),
+//   ],
+// };
+
+
+
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import aspectRatio from '@tailwindcss/aspect-ratio';
+
+const config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -31,13 +62,26 @@ module.exports = {
   ],
   theme: {
     extend: {
-      screens: {
-        xs: "380px",
-        s: "400px"
+      keyframes: {
+        'perspective-change': {
+          '0%': {
+            perspective: '800px',
+            opacity: '0.1',
+          },
+          '100%': {
+            perspective: '2400px',
+            opacity: '1',
+          },
+        },
       },
-    },
+      animation: {
+        'perspective-transition': 'perspective-change 2s ease-in-out forwards',
+      },
+    }
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
+    aspectRatio,
   ],
 };
+
+export default config;
